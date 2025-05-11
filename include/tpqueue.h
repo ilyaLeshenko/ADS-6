aac1f748083c969119e33c610fe810ef600642b7
lesh <113383963+ilyaLeshenko@users.noreply.github.com>
2025-05-11 15:06:18 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,64 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T data) : data(data), next(nullptr) {}
+    };
+    
+    Node* head;
+    Node* tail;
 
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void push(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head || value.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            if (!tail) tail = head;
+            return;
+        }
+        Node* current = head;
+        while (current->next && current->next->data.prior >= value.prior) {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+        if (!newNode->next) {
+            tail = newNode;
+        }
+    }
+    T pop() {
+        if (!head) {
+            throw "Queue is empty!";
+        }        
+        Node* temp = head;
+        T result = temp->data;
+        head = head->next;        
+        if (!head) {
+            tail = nullptr;
+        }        
+        delete temp;
+        return result;
+    }
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+};
 struct SYM {
   char ch;
   int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
